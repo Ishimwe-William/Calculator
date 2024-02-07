f64917b549ecea073317c616e314f7cfb213d838
Ishimwe-William <ishimwe.william2000@gmail.com>
2024-02-07 12:13:04 +0200

    Mobile Programming - Assignment 1

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Switch, SafeAreaView, View, StatusBar as RNStatusBar } from 'react-native';
+import { ThemeContext } from "./src/context/ThemeContext";
+import { myColors } from "./src/style/colors";
+import MyKeyboard from "./src/component/MyKeyboard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <SafeAreaView style={theme === "light" ? styles.container : [styles.container, { backgroundColor: "black" }]}>
+                <RNStatusBar
+                    backgroundColor={theme === "light" ? myColors.light : "black"}
+                    barStyle={theme === "light" ? 'dark-content' : 'light-content'}
+                />
+                <View style={styles.themeSwitchContainer}>
+                    <Switch
+                        value={theme === 'dark'}
+                        onValueChange={toggleTheme}
+                    />
+                </View>
+                <MyKeyboard />
+            </SafeAreaView>
+        </ThemeContext.Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: myColors.light,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    themeSwitchContainer: {
+        marginVertical: 30,
+    },
 });
